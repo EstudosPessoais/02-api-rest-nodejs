b6533b73a7820f265da8df807d0e6914c4377da6
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-01-30 10:24:33 -0300

    feat: adding eslint

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 app.get('/hello', () => {
-    return 'hello world!';
+  return 'hello world!'
 })
 
-app.listen({
-    port: 3333
-}).then(() => {
-    console.log("Http Server running!")
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Http Server running!')
+  })
