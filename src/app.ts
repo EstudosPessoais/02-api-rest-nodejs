c220b3bebbc1645bc75b08c827abd27172e48421
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-02-01 09:34:48 -0300

    feat: adding special config for use test db to exec the e2e tests

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
 
-import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
 
 export const app = fastify()
