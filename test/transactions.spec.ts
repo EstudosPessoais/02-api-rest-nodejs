3dfd2afcf70eca1917fb85f37c6f54344b1feb20
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-01-31 22:06:36 -0300

    feat: adding test listTransactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,7 @@
 import { expect, beforeAll, afterAll, describe, it } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
+import { object } from 'zod'
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
@@ -19,5 +20,29 @@ describe('Transactions routes', () => {
     })
 
     expect(response.statusCode).toEqual(201)
+  })
+
+  it('Should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
